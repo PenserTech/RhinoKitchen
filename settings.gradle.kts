96d9918b5615a30da42f72951e3c8b65b88a67ff
Lance Hill <lance@prometheussoftware.ca>
2025-05-23 14:29:56 -0400

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RhinoKM"
+include(":app")
+include(":feature:recipes")
+include(":feature:orders")
+include(":feature:inventory")
+include(":core:domain")
+include(":core:data")
